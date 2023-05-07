a76ba1bb2eb4f0390d0d59c1705f29d7b93bcfc9
githubMauri <maunz69@gmail.com>
2023-05-07 16:26:52 -0400

    catalogo de productos terminado

--- App.js
+++ App.js
@@ -1,17 +1,21 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Switch, Text, View } from "react-native";
+import { SafeAreaView, StyleSheet, Switch, Text, View } from "react-native";
 import { useColorScheme } from "nativewind";
+import ProductsList from "./components/ProductsList";
 
 export default function App() {
   const { colorScheme, toggleColorScheme } = useColorScheme();
   return (
-    <View className="flex-1 items-center justify-center bg-gray-200 dark:bg-black">
-      <View className="flex-row w-full gap-5">
-        <Text className="text-2xl font-bold dark:text-white">Nueva colección</Text>
+    <SafeAreaView className="flex-1 items-center justify-center bg-gray-200 dark:bg-black">
+      <View className={"flex-row w-full gap-5"}>
+        <Text className="dark:text-white text-2xl font-bold mt-5 p-3">
+          Nueva Colección
+        </Text>
         <Switch value={colorScheme === "dark"} onChange={toggleColorScheme} />
       </View>
-      <StatusBar style={colorScheme==="dark"?"light":"dark"} />
-    </View>
+      <ProductsList />
+      {/* <StatusBar style={colorScheme === "dark" ? "light" : "dark"} /> */}
+    </SafeAreaView>
   );
 }
 
--- components/ProductsList.js
+++ components/ProductsList.js
@@ -1,13 +1,15 @@
 import * as React from "react";
 import { FlatList,Text } from "react-native";
 import { products } from "../products";
+import ProductCard from "./ProductCard";
 
 export default function ProductsLists() {
   return (
     <FlatList
       data={products} // Array de datos
       keyExtractor={(product) => product.id} // Función para obtener el id de cada elemento
-      renderItem={({ item }) => <Text>{item.name}</Text>} // Función para renderizar cada elemento
+      renderItem={({ item }) => <ProductCard {...item}/>} // Función para renderizar cada elemento
+      contentContainerStyle={{ paddingHorizontal: 15 }}
     />
   );
 }
